4c7d326de869954e4f5de4d8027e3b7abec73571
Pecneb <ecneb2000@gmail.com>
2022-06-25 23:39:53 +0200

    lot more commenting for future generations

--- hldnapi.py
+++ hldnapi.py
@@ -1,12 +1,34 @@
+"""
+    Predicting trajectories of objects
+    Copyright (C) 2022 Bence Peter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact email: ecneb2000@gmail.com
+"""
+
 from ctypes import *
 import cv2 as cv
 import darknet
 import argparse
 
+"""Configuration files for darknet"""
 CONFIG = "./darknet_config_files/yolov4.cfg"
 DATA = "./darknet_config_files/coco.data"
 WEIGHTS = "./darknet_config_files/yolov4.weights"
 
+"""Loading configuration files into darknet"""
 network, class_names, colors = darknet.load_network(CONFIG, DATA, WEIGHTS)
 darknet_width = darknet.network_width(network)
 darknet_height = darknet.network_height(network)
@@ -36,11 +58,15 @@ def convert2original(image, bbox):
     return bbox_converted
 
 def detections2cvimg(image):
-    '''
-    image: cv2 image 
-    returns: cv2 image with detections drawn on input image
-    detections: confidence etc.
-    '''
+    """Function to make it easy to use darknet with opencv image class 
+
+    Args:
+        image (Opencv image): input image to run darknet on
+
+    Returns:
+        opencv image: output image of darknet converted to opencv image
+        detections(tuple): detected objects on input image (label, confidence, bbox(x,y,w,h))
+    """
     # Convert frame color from BGR to RGB
     image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
     # Resize image for darknet
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
     Predicting trajectories of objects
-    Copyright (C) <2022>  Bence Peter
+    Copyright (C) 2022 Bence Peter
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -24,9 +24,10 @@ import time
 import numpy as np
 
 def parseArgs():
-    """
-    Read command line arguments
-        input: video source
+    """Function for Parsing Arguments
+
+    Returns:
+        args.input: input video source given in command line argument
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", required=True, help="Path to video.")
@@ -34,18 +35,24 @@ def parseArgs():
     return args.input
 
 def printDetections(detections):
-    """
-    Print labels, confidences and bounding boxes positions of detections
+    """Function for printing out detections
+
+    Args:
+        detections (tuple(label, confidence, bbox(x,y,w,h))): output of darknet detector
     """
     for obj in detections:
         # bbox: x, y, w, h
         print("Class: {}, Confidence: {}, Position: {}".format(obj[0], obj[1], obj[2]))
 
 def getTargets(detections, targetNames=[]):
-    """
-    Retrieve the positions of targeted objects.
-    detections: output of hldnapi.detections2cvimg
-    targetNames: labels of object to be retrieved
+    """Function for extracting targets from detections
+
+    Args:
+        detections (tuple(label, confidence, bbox(x,y,w,h))): output of darknet detector
+        targetNames (list, optional): list of labels, that should be extracted from detections. Defaults to [].
+
+    Returns:
+        list: list of detections with the labels of targetNames
     """
     targets = []
     for label, conf, bbox in detections:
@@ -54,21 +61,37 @@ def getTargets(detections, targetNames=[]):
             targets.append((label, conf, bbox))
     return targets 
 
-def drawCenters(detections, img):
+def updateIMG(detections, IMG):
+    """Functions for drawing a circle in the center of detected objects 
+
+    Args:
+        detections (tuple(label, confidence, bbox(x,y,w,h))): output of darknet detector or getTargets() function
+        IMG (OpenCV IMG object): input image
+    """
     for obj in detections:
-        cv.circle(img, (obj[2][0], obj[2][1]), 2, (0,0,255), 2)
+        cv.circle(IMG, (obj[2][0], obj[2][1]), 2, (0,0,255), 2)
 
 def calcDist(prev, act):
+    """Function to calculate distance between an object on previous frame and actual frame.
+
+    Args:
+        prev (tuple(label, confidence, bbox(x,y,w,h))): Object from previous frame
+        act (tuple(label, confidence, bbox(x,y,w,h))): Object from actual frame
+
+    Returns:
+        xDist, yDist: distance of x coordiantes and distance of y coordiantes
+    """
     xDist = abs(prev[2][0]-act[2][0])
     yDist = abs(prev[2][1]-act[2][1])
     return xDist, yDist
 
 def updateHistory(detections, history, thresh=0.05):
-    """
-    updates the history of objects
-    if the old obj center's and new obj center's distance is within the threshold,
-    then add the new one to the old one's history,
-    but if no close centerpoint is found, make new history for the obj
+    """Function to update object history
+
+    Args:
+        detections (list of (label, confidence, bbox)): output of darknet
+        history (2D matrix): objects's history, each row is a history of an object
+        thresh (float, optional): The percentage of max distance. Defaults to 0.05.
     """
     for new in detections:
         added = False
@@ -82,22 +105,29 @@ def updateHistory(detections, history, thresh=0.05):
 
 def main():
     input = parseArgs()
+
+    # only initialize darknet when theres valid input video
     if input is not None:
         import hldnapi
 
     try:
-        # check, if input arg is a webcam
+        # if input source is number, then it is a webcam 
         input = int(input)
     except ValueError:
         print("Input source is a Video.")
 
     cap = cv.VideoCapture(input)
     if not cap.isOpened():
-        print("Source cannot be opened.")
+        print("Video cannot be opened.")
         exit(0) 
 
+    # defining history list for later usage
+    # 2D MTX: rows are the objects and columns are the timeline
+    # obj1 1. position 2. position ...
+    # obj2 1. pos 2. pos ...
     history = []
 
+    # playing video frame by frame until 'q' is pressed or video ends
     while(1):
         ret, frame = cap.read()
         if frame is None:
@@ -106,27 +136,39 @@ def main():
         # time before computation
         prev_time = time.time()
     
+        # using darknet on next video frame
         I, detections = hldnapi.detections2cvimg(frame)
-    
+        
         # calculating fps from time before computation and time now
         fps = int(1/(time.time() - prev_time))
-        
+
+        # extracting only the objects with the inputted label
         targets = getTargets(detections, targetNames=("person", "car"))
 
+        # update history for tracking objects
         updateHistory(targets, history)
 
         # printDetections(targets)
 
-        drawCenters(targets, I)
+        # draw red dots on selected objects center for testing purposes
+        updateIMG(targets, I)
 
-        cv.circle(I, (history[3][-1][2][0],history[3][-1][2][1]), 2, (255,0,0), 2)
+        # marking an object with blue dot for testing purposes
+        cv.circle(I, (history[1][-1][2][0],history[1][-1][2][1]), 2, (255,0,0), 2)
 
         cv.imshow("FRAME", I)
-    
+
+        # printing out fps to stdout
         print("FPS: {}".format(fps))
 
-        # print(history[1][-2:-1])
+        # printing out the length of the longest history for testing purposes
+        longestHist = history[0]
+        for objHistory in history:
+            if len(objHistory) > len(longestHist):
+                longestHist = objHistory
+                print(len(longestHist))
 
+        # press 'q' to quit
         if cv.waitKey(1) == ord('q'):
             break
     cap.release()
