55734f63270be7e6516a584b05d00d5a87531b20
Pecneb <ecneb2000@gmail.com>
2022-07-09 23:01:52 +0200

    HistoryClass.py: using self.isMoving flag to determine if object is moving.

--- historyClass.py
+++ historyClass.py
@@ -88,4 +88,4 @@ class TrackedObject():
         if self.history:
             # calculating euclidean distance of the first stored detection and last stored detection
             # this is still hard coded, so its a bit hacky, gotta find a good metric to tell if an object is moving or not
-            self.isMoving = ((self.history[0].X-self.history[-1].X)**2 + (self.history[0].Y-self.history[-1].Y)**2)**(1/2) > 5.0
+            self.isMoving = ((self.history[0].X-self.history[-1].X)**2 + (self.history[0].Y-self.history[-1].Y)**2)**(1/2) > 3.0
