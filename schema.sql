a84c2c2ec6a5c08d474eee8228963cd84a76a8e4
Pecneb <ecneb2000@gmail.com>
2022-07-21 22:07:32 +0200

    Updating database schema, making it more tidy

--- schema.sql
+++ schema.sql
@@ -1,10 +1,29 @@
+CREATE TABLE IF NOT EXISTS objects (
+    objID INTEGER PRIMARY KEY NOT NULL,
+    label TEXT NOT NULL
+);
 CREATE TABLE IF NOT EXISTS detections (
-                                objID INTEGER PRIMARY KEY NOT NULL,
-                                frameNum INTEGER NOT NULL,
-                                label TEXT NOT NULL,
-                                confidence REAL NOT NULL,
-                                x REAL NOT NULL,
-                                y REAL NOT NULL,
-                                width REAL NOT NULL,
-                                height REAL NOT NULL
-                            );
+                objID INTEGER NOT NULL,
+                frameNum INTEGER NOT NULL,
+                confidence REAL NOT NULL,
+                x REAL NOT NULL,
+                y REAL NOT NULL,
+                width REAL NOT NULL,
+                height REAL NOT NULL,
+                vx REAL NOT NULL,
+                vy REAL NOT NULL,
+                FOREIGN KEY(objID) REFERENCES objects(objID)
+            );
+CREATE TABLE IF NOT EXISTS predictions (
+                objID INTEGER NOT NULL,
+                frameNum INTEGER NOT NULL,
+                idx INTEGER NOT NULL,
+                x REAL NOT NULL,
+                y REAL NOT NULL
+            );
+CREATE TABLE IF NOT EXISTS metadata (
+                historyDepth INTEGER NOT NULL,
+                futureDepth INTEGER NOT NULL,
+                yoloVersion INTEGER NOT NULL,   
+                device TEXT NOT NULL
+            );
