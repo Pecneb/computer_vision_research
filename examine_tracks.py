f142764a64424350e1108af7716d4edffdde58a4
Pecneb <ecneb2000@gmail.com>
2023-02-05 14:52:43 +0100

    Implemented first version of trajectory examiner script.

---
+++ examine_tracks.py
@@ -0,0 +1,188 @@
+from dataManagementClasses import TrackedObject
+from processing_utils import load_joblib_tracks
+from argparse import ArgumentParser
+import cv2
+import numpy as np
+
+def upscalebbox(bbox, fwidth, fheight):
+    """Upscale normalized coordinates to the video's frame size.
+    The downscaling method was: X = X * fwidth / (fwidth/fheight), 
+                                Y = Y * fheight
+                                W = W * fwidth / (fwidth/fheight), 
+                                H = H * fheight
+
+    Args:
+        bbox (tuple): Tuple of 4 values (X,Y,W,H) 
+        fwidth (int): Frame width of the video. 
+        fheight (_type_): Frame height of the video. 
+    """
+    ratio = fwidth / fheight
+    X, Y, W, H = bbox
+    X = (X * fwidth) / ratio 
+    W = (W * fwidth) / ratio
+    Y = Y * fheight 
+    H = H * fheight 
+    return X, Y, W, H
+
+def bbox2points(bbox):
+    """
+    From bounding box yolo format
+    to corner points cv2 rectangle
+    """
+    x, y, w, h = bbox
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+def drawbbox(bbox: tuple, image: np.ndarray):
+    """Draw bounding box of an object to the given image.
+
+    Args:
+        detection (Detection): Detection object. 
+        image (np.ndarray): OpenCV image object.
+    """
+    ret_img = image.copy()
+    bboxUpscaled = upscalebbox(bbox, image.shape[1], image.shape[0])
+    left, top, right, bottom = bbox2points(bboxUpscaled)
+    cv2.rectangle(ret_img, (left, top), (right, bottom), (0,255,0), 1)
+    return ret_img
+
+def database_is_joblib(path: str):
+    return path.split('.')[-1] == "joblib"
+
+def draw_trajectory(img, track: TrackedObject, upscale: bool = True):
+    X = track.history_X
+    Y = track.history_Y
+    if upscale:
+        aspect_ratio = img.shape[1] / img.shape[0]
+        X = X * (img.shape[1] / aspect_ratio)
+        Y = Y * img.shape[0]
+    ret_img = img.copy()
+    pts = np.column_stack((X, Y)).astype(int) 
+    cv2.polylines(ret_img, [pts], False, (0,255,0))
+    return ret_img
+
+def next_frame_id(i_frame: int, max_i: int):
+    """If actial frame id is less or equal to the max id,
+    then return next frame id, -1 otherwise.
+
+    Args:
+        i_frame (int): Actual frame id. 
+        max_i (int): Maximum limit frame id. 
+
+    Returns:
+        int: Next frame id or -1, if there is no next frame. 
+    """
+    ret_i = i_frame + 1
+    if ret_i > max_i:
+        return -1
+    return ret_i
+
+def previous_frame_id(i_frame: int, min_i: int):
+    """If actial frame id is larger than minimum id,
+    then return previous frame id, -1 otherwise.
+
+    Args:
+        i_frame (int): Actual frame id. 
+        min_i (int): Minimum limit frame id. 
+
+    Returns:
+        int: Previous frame id or -1, if there is no previous frame. 
+    """
+    ret_i = i_frame - 1
+    if ret_i < min_i:
+        return -1
+    return ret_i
+
+def examine_tracks(args):
+    if not database_is_joblib(args.database):
+        raise IOError(("Not joblib extension."))
+    tracks = load_joblib_tracks(args.database)
+    
+    i_track = 0
+    while i_track >= 0 and i_track < len(tracks):
+        video = cv2.VideoCapture(args.video)
+        if not video.isOpened():
+            raise IOError("Can not open video.")
+
+        start_frame = tracks[i_track].history[0].frameID
+        video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+        act_start_frame = video.get(cv2.CAP_PROP_POS_FRAMES)
+
+        if act_start_frame == start_frame:
+            window_name = f"Object ID{tracks[i_track].objID}"
+            cv2.namedWindow(window_name)
+            i_det = 0
+            while i_det < tracks[i_track].history_X.shape[0]:
+                ret, frame = video.read()
+                if not ret:
+                    break
+
+                frame_traj = draw_trajectory(frame, tracks[i_track])
+                frame_traj = drawbbox(
+                    (tracks[i_track].history_X[i_det],
+                    tracks[i_track].history_Y[i_det],
+                    tracks[i_track].history[i_det].Width,
+                    tracks[i_track].history[i_det].Height),
+                    frame_traj
+                )
+                cv2.imshow(window_name, frame_traj)
+                i_det += 1
+                key_2 =cv2.waitKey(0)
+                if  key_2 == ord('s'):
+                    continue
+                if key_2 == ord('b') or key_2 == ord('q') or key_2 == ord('n'):
+                    break
+        else:
+            i_track += 1
+            cv2.destroyAllWindows()
+            continue
+
+        key = cv2.waitKey(0)
+        if key == ord('q'):
+            cv2.destroyAllWindows()
+            break
+        if key == ord('r'):
+            cv2.destroyAllWindows()
+            continue
+        if key == ord('n'):
+            if i_track + 1 < len(tracks):
+                i_track += 1
+            else:
+                print("Cant access next track.")
+            cv2.destroyAllWindows()
+            continue
+        if key == ord('b'):
+            if i_track - 1 >= 0:
+                i_track -= 1
+            else:
+                print("Cant access previous track.")
+            cv2.destroyAllWindows()
+            continue
+            
+
+def main():
+    argparser = ArgumentParser(
+        prog="Program to examine trajectories individually.",
+    )
+    argparser.add_argument(
+        "-db", "--database",
+        type=str,
+        required=True,
+        help="Path to joblib database file."
+    )
+    argparser.add_argument(
+        "--video",
+        type=str,
+        required=True,
+        help="Path to video srouce."
+    )
+    argparser.set_defaults(func=examine_tracks)
+
+    args = argparser.parse_args()
+    args.func(args)
+
+if __name__ == "__main__":
+    main()
