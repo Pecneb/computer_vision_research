0170e2af97e1a13af8c8c82e5f5161419c30e381
Pecneb <ecneb2000@gmail.com>
2023-03-05 15:17:16 +0100

    feat: more test cases added for more coverage

--- tests/test_insert_weights_func.py
+++ tests/test_insert_weights_func.py
@@ -6,7 +6,7 @@ class TestInsertWeightsFunc:
     Y = np.array([1,2,3,4,5,6,7,8,9,10])
 
     def test_insert_func_n_weights_2(self):
-        expected = np.array([1,1,2,2,6,6,10,10])
+        expected = np.array([1,1,5,5,9,9,10,10])
         midx = 1
         endidx = 9 
         n_weights = 2
@@ -15,10 +15,10 @@ class TestInsertWeightsFunc:
         feature_vector = insert_weights_into_feature_vector(
             midx, endidx, n_weights, self.X, self.Y, 2, feature_vector
         )
-        np.testing.assert_array_equal(expected, feature_vector)
+        np.testing.assert_array_equal(feature_vector, expected)
 
     def test_insert_func_n_weights_4(self):
-        expected = np.array([1,1,2,2,4,4,6,6,8,8,10,10])
+        expected = np.array([1,1,3,3,5,5,7,7,9,9,10,10])
         midx = 1
         endidx = 9 
         n_weights = 4 
@@ -27,4 +27,16 @@ class TestInsertWeightsFunc:
         feature_vector = insert_weights_into_feature_vector(
             midx, endidx, n_weights, self.X, self.Y, 2, feature_vector
         )
-        np.testing.assert_array_equal(expected, feature_vector)
+        np.testing.assert_array_equal(feature_vector, expected)
+
+    def test_insert_func_n_weights_4_midx_5(self):
+        expected = np.array([1,1,6,6,7,7,8,8,9,9,10,10])
+        midx = 5
+        endidx = 9 
+        n_weights = 4 
+        feature_vector = np.array([self.X[0], self.Y[0],
+                                   self.X[endidx], self.Y[endidx]])
+        feature_vector = insert_weights_into_feature_vector(
+            midx, endidx, n_weights, self.X, self.Y, 2, feature_vector
+        )
+        np.testing.assert_array_equal(feature_vector, expected)
--- tests/test_make_features_version_five.py
+++ tests/test_make_features_version_five.py
@@ -5,16 +5,101 @@ from dataManagementClasses import TrackedObject, Detection
 class TestFeatureVectorV5:
     trackedObjects = np.array([
         trackedObjectFactory((
-            [detectionFactory(1, 1, "car", 0.75, 0.5, 0.75, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0), detectionFactory(1, 2, "car", 0.75, 0.51, 0.75, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0), 
-             detectionFactory(1, 3, "car", 0.75, 0.53, 0.75, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0), detectionFactory(1, 4, "car", 0.75, 0.5, 0.75, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0)], 
-            np.array([0.5, 0.51, 0.53, 0.54, 0.56]), np.array([0.75, 0.75, 0.75, 0.75, 0.75]), np.array([0.0, 0.01, 0.02, 0.75, 0.0]), np.array([0.0, 0.0, 0.0, 0.0, 0.0]), np.array([0.0, 0.01, 0.01, 0.0, 0.0]), np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+            [detectionFactory(1, 1, "car", 0.75, 0,0,0,0,0,0,0,0) for i in range(100)], 
+            np.linspace(0, 1.5, 100),
+            np.linspace(0, 1.5, 100),
+            np.linspace(0, 1.5, 100),
+            np.linspace(0, 1.5, 100),
+            np.linspace(0, 1.5, 100),
+            np.linspace(0, 1.5, 100)
         ))
     ])
 
     def test_make_feature_vectors_version_five(self):
-        X, y, metadata = make_feature_vectors_version_five(self.trackedObjects, np.array([0]), 3, 1)
-        X_expected = np.array([[0.5, 0.75, 0.51, 0.75, 0.53, 0.75],
-                               [0.51, 0.75, 0.53, 0.75, 0.54, 0.75]])
-        y_expected = np.array([0,0])
-        np.testing.assert_array_equal(X, X_expected)
-        np.testing.assert_array_equal(y, y_expected)
+        x, y, metadata = make_feature_vectors_version_five(self.trackedObjects, np.array([0]), 15, 5)
+        x_expected_sample = np.array([self.trackedObjects[0].history_X[0],
+                                 self.trackedObjects[0].history_Y[0],
+                                 self.trackedObjects[0].history_X[9],
+                                 self.trackedObjects[0].history_Y[9],
+                                 self.trackedObjects[0].history_X[10],
+                                 self.trackedObjects[0].history_Y[10],
+                                 self.trackedObjects[0].history_X[11],
+                                 self.trackedObjects[0].history_Y[11],
+                                 self.trackedObjects[0].history_X[12],
+                                 self.trackedObjects[0].history_Y[12],
+                                 self.trackedObjects[0].history_X[13],
+                                 self.trackedObjects[0].history_Y[13],
+                                 self.trackedObjects[0].history_X[14],
+                                 self.trackedObjects[0].history_Y[14],])
+        np.testing.assert_equal(x.shape[1], x_expected_sample.shape[0])
+        np.testing.assert_array_equal(x[0], x_expected_sample)
+
+    def test_make_feature_vectors_version_five_50(self):
+        x, y, metadata = make_feature_vectors_version_five(self.trackedObjects, np.array([0]), 15, 5)
+        x_expected_sample = np.array([self.trackedObjects[0].history_X[50],
+                                 self.trackedObjects[0].history_Y[50],
+                                 self.trackedObjects[0].history_X[59],
+                                 self.trackedObjects[0].history_Y[59],
+                                 self.trackedObjects[0].history_X[60],
+                                 self.trackedObjects[0].history_Y[60],
+                                 self.trackedObjects[0].history_X[61],
+                                 self.trackedObjects[0].history_Y[61],
+                                 self.trackedObjects[0].history_X[62],
+                                 self.trackedObjects[0].history_Y[62],
+                                 self.trackedObjects[0].history_X[63],
+                                 self.trackedObjects[0].history_Y[63],
+                                 self.trackedObjects[0].history_X[64],
+                                 self.trackedObjects[0].history_Y[64],])
+        np.testing.assert_equal(x.shape[1], x_expected_sample.shape[0])
+        np.testing.assert_array_equal(x[50], x_expected_sample)
+
+    def test_make_feature_vectors_version_five_30(self):
+        x, y, metadata = make_feature_vectors_version_five(self.trackedObjects, np.array([0]), 15, 5)
+        x_expected_sample = np.array([self.trackedObjects[0].history_X[30],
+                                 self.trackedObjects[0].history_Y[30],
+                                 self.trackedObjects[0].history_X[39],
+                                 self.trackedObjects[0].history_Y[39],
+                                 self.trackedObjects[0].history_X[40],
+                                 self.trackedObjects[0].history_Y[40],
+                                 self.trackedObjects[0].history_X[41],
+                                 self.trackedObjects[0].history_Y[41],
+                                 self.trackedObjects[0].history_X[42],
+                                 self.trackedObjects[0].history_Y[42],
+                                 self.trackedObjects[0].history_X[43],
+                                 self.trackedObjects[0].history_Y[43],
+                                 self.trackedObjects[0].history_X[44],
+                                 self.trackedObjects[0].history_Y[44],])
+        np.testing.assert_equal(x.shape[1], x_expected_sample.shape[0])
+        np.testing.assert_array_equal(x[30], x_expected_sample)
+
+    def test_make_feature_vectors_version_five_stride_10(self):
+        x, y, metadata = make_feature_vectors_version_five(self.trackedObjects, np.array([0]), 10, 5)
+        x_expected_sample = np.array([self.trackedObjects[0].history_X[30],
+                                 self.trackedObjects[0].history_Y[30],
+                                 self.trackedObjects[0].history_X[34],
+                                 self.trackedObjects[0].history_Y[34],
+                                 self.trackedObjects[0].history_X[35],
+                                 self.trackedObjects[0].history_Y[35],
+                                 self.trackedObjects[0].history_X[36],
+                                 self.trackedObjects[0].history_Y[36],
+                                 self.trackedObjects[0].history_X[37],
+                                 self.trackedObjects[0].history_Y[37],
+                                 self.trackedObjects[0].history_X[38],
+                                 self.trackedObjects[0].history_Y[38],
+                                 self.trackedObjects[0].history_X[39],
+                                 self.trackedObjects[0].history_Y[39],])
+        np.testing.assert_equal(x.shape[1], x_expected_sample.shape[0])
+        np.testing.assert_array_equal(x[30], x_expected_sample)
+
+    def test_make_feature_vectors_version_five_stride_10_n_weights_3(self):
+        x, y, metadata = make_feature_vectors_version_five(self.trackedObjects, np.array([0]), 10, 2)
+        x_expected_sample = np.array([self.trackedObjects[0].history_X[30],
+                                 self.trackedObjects[0].history_Y[30],
+                                 self.trackedObjects[0].history_X[36],
+                                 self.trackedObjects[0].history_Y[36],
+                                 self.trackedObjects[0].history_X[38],
+                                 self.trackedObjects[0].history_Y[38],
+                                 self.trackedObjects[0].history_X[39],
+                                 self.trackedObjects[0].history_Y[39],])
+        np.testing.assert_equal(x.shape[1], x_expected_sample.shape[0])
+        np.testing.assert_array_equal(x[30], x_expected_sample)
