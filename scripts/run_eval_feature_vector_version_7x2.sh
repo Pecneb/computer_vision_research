1ab1f4146e11f301b4df8715d4047ad9fda1b940
Pecneb <ecneb2000@gmail.com>
2023-10-23 18:20:54 +0200

    feat: scripts

--- scripts/run_eval_feature_vector_version_7x2.sh
+++ scripts/run_eval_feature_vector_version_7x2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python3 -m sklearnex computer_vision_research/classification.py \
-    --n-jobs 14 \
+    --n-jobs 7 \
     train \
     --dataset \
     ../../cv_research_video_dataset/Bellevue_116th_NE12th_24h/Preprocessed_threshold_0.7_enter-exit-distance_0.1/ \
@@ -9,29 +9,31 @@ python3 -m sklearnex computer_vision_research/classification.py \
     ../../cv_research_video_dataset/Bellevue_116th_NE12th_24h/Preprocessed_threshold_0.7_enter-exit-distance_0.1/ \
     --preprocessed \
     --min-samples 100 --max-eps 0.15 \
-    --mse 0.2 --feature-version 7
+    --mse 0.2 --feature-version 7x2
 python3 -m sklearnex computer_vision_research/classification.py \
-    --n-jobs 14 \
+    --n-jobs 7 \
     train \
     --dataset \
     ../../cv_research_video_dataset/Bellevue_Eastgate_24h/Preprocessed/ \
     --output \
     ../../cv_research_video_dataset/Bellevue_Eastgate_24h/Preprocessed/ \
     --preprocessed \
+    --test 0.5 \
     --min-samples 200 --max-eps 0.16 \
-    --mse 0.2 --feature-version 7
+    --mse 0.2 --feature-version 7x2
 python3 -m sklearnex computer_vision_research/classification.py \
-    --n-jobs 14 \
+    --n-jobs 7 \
     train \
     --dataset \
     ../../cv_research_video_dataset/Bellevue_150th_Newport_24h_v2/Preprocessed/ \
     --output \
     ../../cv_research_video_dataset/Bellevue_150th_Newport_24h_v2/Preprocessed/ \
     --preprocessed \
+    --test 0.5 \
     --min-samples 200 --max-eps 0.1 \
-    --mse 0.2 --feature-version 7
+    --mse 0.2 --feature-version 7x2
 python3 -m sklearnex computer_vision_research/classification.py \
-    --n-jobs 14 \
+    --n-jobs 7 \
     train \
     --dataset \
     ../../cv_research_video_dataset/Bellevue_150th_SE38th_24h/Preprocessed/ \
@@ -40,15 +42,15 @@ python3 -m sklearnex computer_vision_research/classification.py \
     --preprocessed \
     --test 0.5 \
     --min-samples 100 --max-eps 0.15 \
-    --mse 0.2 --feature-version 7
+    --mse 0.2 --feature-version 7x2
 python3 -m sklearnex computer_vision_research/classification.py \
-    --n-jobs 14 \
+    --n-jobs 7 \
     train \
     --dataset \
     ../../cv_research_video_dataset/Bellevue_NE8th_24h_v2/Preprocessed_threshold_0.7_enter-exit-distance_1.0/ \
     --output \
     ../../cv_research_video_dataset/Bellevue_NE8th_24h_v2/Preprocessed_threshold_0.7_enter-exit-distance_1.0/ \
     --preprocessed \
-    --test 0.5 \
+    --test 0.65 \
     --min-samples 400 --max-eps 0.15 \
-    --mse 0.2 --feature-version 7
+    --mse 0.2 --feature-version 7x2
